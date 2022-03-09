12d085e08db8e512145c58145e214ca79a9a374f
rasputinforever <erik.justin.portillo@gmail.com>
2022-03-08 21:12:58 -0800

    async

--- routes/main.js
+++ routes/main.js
@@ -2,11 +2,15 @@ const choreTracker = require('../models/choreTracker.js')
 
 module.exports = (app) => {
   // full worklist given query params by user
-  app.get('/api/chores', (req, res) => {    
+  app.get('/api/chores', async (req, res) => {    
     const paramObj = JSON.parse(req.query.q)    
     console.log("GET request on chores API", "User IP:", req.headers['x-forwarded-for'] || req.connection.remoteAddress, "User:", paramObj.user)      
-    const data = choreTracker.getChores()
-    res.status(200).json(data)
+    choreTracker.getChores()
+    .then(data => {
+      console.log("Sending this", data)
+      res.status(200).json(data)
+    })
+    
   })
 
   app.put('/api/chores', (req, res) => {        
@@ -14,6 +18,7 @@ module.exports = (app) => {
     console.log("PUT request on chores API", "User IP:", req.headers['x-forwarded-for'] || req.connection.remoteAddress, "User:", paramObj.user)      
     
     const data = choreTracker.writeChore(paramObj.data)
+
     res.status(201).json(data)
     
   });
