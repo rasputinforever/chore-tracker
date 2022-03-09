4beb0f3a4054569c880703b2c2c24282eb8741a6
rasputinforever <erik.justin.portillo@gmail.com>
2022-03-08 21:12:51 -0800

    promise-ify

--- models/choreTracker.js
+++ models/choreTracker.js
@@ -1,16 +1,51 @@
 const fs = require('fs');
+const { resolve } = require('path');
 var path = require('path');
 
 const choreTracker = {
     getChores: () => {
-
-        return []
+        return new Promise((resolve, reject) => {
+            fs.readFile(__dirname + '/chores.json', 'utf8' , (err, data) => {
+                if (err) {
+                    console.error("error reading file", err);
+                    resolve([])
+                    
+                }
+                    console.log("This is the file", JSON.parse(data))
+                    resolve(JSON.parse(data))
+            });
+        })
+        
     }
     ,
-    writeChore: (data) => {
-        console.log("Got a chore", data)
-        return []
-    }
+    writeChore: (received) => {
+        console.log("Got a chore", received)
+        return new Promise((resolve, reject) => {
+            fs.readFile(__dirname + '/chores.json', 'utf8' , (err, data) => {
+                if (err) {
+                    console.error("error reading file", err);
+                    resolve(false)                
+                }
+    
+                const newData = JSON.parse(data)
+                console.log("This is the file", newData)
+                newData.push(received)
+                console.log("Will PUsh This", newData)
+    
+                fs.writeFile(__dirname + '/chores.json', JSON.stringify(newData), err => {
+                    if (err) {
+                        console.error("Could not write file", err)
+                        resolve(false)                
+                    }
+                    console.log("Added Chore")
+                    resolve(newData)
+                })
+                    
+            });
+            
+        }
+        
+    )}
     ,
     checkChores: () => {
         fs.readFile(__dirname + '/chores.json', 'utf8' , (err, data) => {
